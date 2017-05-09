cc8c843837ef3ce53f94b8e31c203172567849a5
Daviid21 <dmari.scfe11@gmail.com>
2017-05-09 19:16:54 +0200

    Ventana Realizada

--- src/Ventana/VentanaLetraDNI.java
+++ src/Ventana/VentanaLetraDNI.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Component;
+import javax.swing.JButton;
 
 
 /*
@@ -18,6 +24,8 @@ import javax.swing.border.EmptyBorder;
 public class VentanaLetraDNI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 
 
@@ -29,8 +37,29 @@ public class VentanaLetraDNI extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		
+		JLabel lblIntroduceDni = new JLabel("INTRODUCE DNI (SIN LETRA)");
+		lblIntroduceDni.setAlignmentX(0.5f);
+		lblIntroduceDni.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblIntroduceDni);
+		
+		textField = new JTextField();
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblLetraDniCalculada = new JLabel("Letra DNI Calculada");
+		lblLetraDniCalculada.setAlignmentX(0.5f);
+		contentPane.add(lblLetraDniCalculada);
+		
+		textField_1 = new JTextField();
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnCalcular = new JButton("Calcular");
+		btnCalcular.setAlignmentX(Component.CENTER_ALIGNMENT);
+		contentPane.add(btnCalcular);
 	}
 
 }
